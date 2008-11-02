19a8f8feb10e32fbb644eeaa7980426bd61fc66a
opsoneo <opsoneo@64bf782a-a8ed-11dd-a714-1752b6e4c161>
2008-11-02 15:00:03 +0000

    Initial upload

---
+++ HelloWorld/MainView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Neonode.Forms;
+
+namespace HelloWorld
+{
+    class MainView : CustomView
+    {
+        private string sText = "";
+        private int iClicks = 0;
+
+        public string Text
+        {
+            get { return sText; }
+            set { sText = value; }
+        }
+
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Title.Text = "Hello World";
+            this.Description.Text = "Neonode.Forms.CustomView example";
+            this.ToolsSweep.Enabled = true;
+            this.ToolsSweep.Occurred += new System.ComponentModel.CancelEventHandler(ToolsSweep_Occurred);
+            this.Paint += new EventHandler<PaintEventArgs>(MainView_Paint);
+            this.Click += new EventHandler<System.Windows.Forms.MouseEventArgs>(MainView_Click);
+        }
+
+        private void ToolsSweep_Occurred(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MenuView menuView = new MenuView();
+            menuView.ShowDialog();
+        }
+
+        private void MainView_Paint(object sender, PaintEventArgs e)
+        {
+            Font font = new Font("Tahoma", 12f, FontStyle.Regular);
+            Graphics graphics = e.Graphics;
+            graphics.DrawString("Number of clicks: " + iClicks.ToString(), font, new SolidBrush(this.Title.TextColor), (float)8, (float)30);
+        }
+
+        void MainView_Click(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            iClicks++;
+            this.Refresh();
+        }
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Windows.Forms;
+using Neonode.Forms;
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [MTAThread]
+        static void Main()
+        {
+            string windowCaption = "C71F25F8-ECFF-3d90-92DF-D6D977CDDA72";    // IMPORTANT -> create a new guid and insert it here
+            using (SingleInstance instance = new SingleInstance(windowCaption))
+            {
+                if (instance.AlreadyRunning)
+                {
+                    instance.TryActivatePreviousInstance(windowCaption);
+                }
+                else
+                {
+                    MainView mainView = new MainView();
+                    mainView.Text = windowCaption;
+
+                    mainView.ShowDialog();
+                }
+            }
+        }
+    }
+}

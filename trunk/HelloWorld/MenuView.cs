325420fa08c9f37f7d0ec10f888e13e7dd553b8c
opsoneo <opsoneo@64bf782a-a8ed-11dd-a714-1752b6e4c161>
2008-11-02 15:00:03 +0000

    Initial upload

---
+++ trunk/HelloWorld/MenuView.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+using Neonode.Forms;
+
+namespace HelloWorld
+{
+    class MenuView : Menu
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            ImageList imgList = new ImageList();
+            imgList.Images.Add(base.GetType().Assembly, "HelloWorld.Properties.Resources.resources", "Settings");
+            imgList.Images.Add(base.GetType().Assembly, "HelloWorld.Properties.Resources.resources", "About");
+            this.ImageList = imgList;
+            MenuPage menuPage = new MenuPage();
+            menuPage.Buttons[0].Text = "Settings";
+            menuPage.Buttons[0].ImageIndex = 0;
+            menuPage.Buttons[0].Enabled = true;
+            menuPage.Buttons[0].Click += new System.ComponentModel.CancelEventHandler(MenuViewSettings_Click);
+            menuPage.Buttons[1].Text = "About";
+            menuPage.Buttons[1].ImageIndex = 1;
+            menuPage.Buttons[1].Enabled = true;
+            menuPage.Buttons[1].Click += new System.ComponentModel.CancelEventHandler(MenuViewAbout_Click);
+            this.TabPages.Add(menuPage);
+        }
+
+        void MenuViewSettings_Click(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SettingsView settingsView = new SettingsView();
+            settingsView.ShowDialog();
+        }
+
+        void MenuViewAbout_Click(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBox.Show(this.Handle, "Version: " + Assembly.GetExecutingAssembly().GetName().Version.ToString() + "\r\n\r\nHelloWorld example", "About HelloWorld");
+        }
+    }
+}
---
+++ trunk/HelloWorld/SettingsView.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Text;
+using Neonode.Forms;
+
+namespace HelloWorld
+{
+    class SettingsView : ListView
+    {
+        public SettingsView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            this.Title.Text = "ListView example";
+            
+            ImageList imgList = new ImageList();
+            imgList.Images.Add(base.GetType().Assembly, "HelloWorld.Properties.Resources.resources", "Happy");
+            imgList.Images.Add(base.GetType().Assembly, "HelloWorld.Properties.Resources.resources", "Smile");
+            imgList.Images.Add(base.GetType().Assembly, "HelloWorld.Properties.Resources.resources", "Tongue");
+            this.ImageList = imgList;
+
+            ListViewItem listItem1 = new ListViewItem("Example item 1", 0, "Example item 1 selected");
+            ListViewItem listItem2 = new ListViewItem("Example item 2", 1, "Example item 2 selected");
+            ListViewItem listItem3 = new ListViewItem("Example item 3", 2, "Example item 3 selected");
+
+            Items.Add(listItem1);
+            Items.Add(listItem2);
+            Items.Add(listItem3);
+
+            this.Click += new EventHandler(SettingsView_Click);
+
+        }
+
+        void SettingsView_Click(object sender, EventArgs e)
+        {
+            switch (this.SelectedItem.Index)
+            {
+                case 0:
+                    MessageBox.Show(this.Handle, "You clicked on example item 1");
+                    break;
+
+                case 1:
+                    MessageBox.Show(this.Handle, "You clicked on example item 2");
+                    break;
+
+                case 2:
+                    MessageBox.Show(this.Handle, "You clicked on example item 3");
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
